df9a93e58f2d228ee645f929fece9924daf86cc8
ChrisLCoop <christian.limas.e@gmail.com>
2023-12-22 21:34:36 -0500

    avance05

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-//prueba
+
+document.getElementById("menu").addEventListener('click' ,mostrar_menu);
+
+function mostrar_menu(){
+    document.getElementById("nav").classList.toggle("ocultar-menu")
+}
